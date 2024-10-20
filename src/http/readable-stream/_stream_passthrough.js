bda6bd5dc06e901f9ee1bdc4dde383f7153f54eb
Curt Tudor <curt@rentallect.com>
2024-10-20 08:24:29 -0600

    introduce to 'target host transformer' to gzip inflator

--- src/http/readable-stream/_stream_passthrough.js
+++ src/http/readable-stream/_stream_passthrough.js
@@ -5,11 +5,11 @@
 import { Transform } from './_stream_transform';
 import EventEmitter from 'events';
 import pako from 'pako';
-import { isEqual } from 'lodash-es';
+import { isEqual, isUndefined } from 'lodash-es';
 
 class PassThrough extends Transform  {
 
-  constructor (options) {
+  constructor (options, zitiOptions) {
     super(options);
     this.ee = new EventEmitter();
     if (options.headers) {
@@ -23,12 +23,28 @@ class PassThrough extends Transform  {
         }
       }
     }
+    this.zitiContext = zitiOptions.zitiContext;
   }
 
   _transform(chunk, encoding, cb) {
     if (this.isGzip) {
       this.inflator.push(chunk);
       chunk = this.inflator.result;
+
+      /**
+       * This logic was added on behalf of Solarwinds Orion, which embeds the host:port of the protected web server in some 
+       * HTML/inline-JS (which is also gzip'ed). We must swap the hostname from the "protected web server" to the boot-strapper, 
+       * or some downstream Orion logic will mismatch when doing some hostname comparisons, which leads it to NOT include 
+       * an XSRF Token header on HTTP requests, which leads to 400's being returned fro teh web server, 
+       * which leads to errors in Orion web UI.
+       */
+      if (!isUndefined(chunk)) {
+        let decodedChunk = new TextDecoder().decode(chunk);
+        if (decodedChunk.indexOf(this.zitiContext.targetServiceHostAndPort) != -1) {
+          decodedChunk = decodedChunk.replaceAll(this.zitiContext.targetServiceHostAndPort, this.zitiContext.bootstrapperHost);
+          chunk = new TextEncoder().encode(decodedChunk);
+        }
+      }
     }
     cb(null, chunk, this);
   };
